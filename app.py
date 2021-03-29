5ba1eeafe4da7361791c82328c40b3ccdbed31b9
Sharon-B <sharonbagnall@gmail.com>
2021-03-29 11:22:51 +0000

    Initial setup of Flask app

---
+++ app.py
@@ -0,0 +1,21 @@
+import os
+from flask import Flask
+if os.path.exists("env.py"):
+    import env
+
+
+# Create instance of Flask
+app = Flask(__name__)
+
+
+# Tester function
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+
+# Set how & where to run the app
+if __name__ == "__main__":
+    app.run(host=os.environ.get("IP"),
+            port=int(os.environ.get("PORT")),
+            debug=True)     # change to debug=False when in production mode

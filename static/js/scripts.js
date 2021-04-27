a04c4e00a396ca0b2ca610d95f5e800a31ab92e6
Sharon-B <sharonbagnall@gmail.com>
2021-04-27 19:45:10 +0000

    Create separate emailjs.js file for the emailjs code

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,18 +1,1 @@
-// Emailjs:
-window.onload = function() {
-    document.getElementById('contact-form').addEventListener('submit', function(event) {
-        event.preventDefault();
-        emailjs.sendForm('contact_service', 'contact_form', this)
-            .then(function() {
-                $('#email-confirm-msg').html("Thank you for your email, we will be in touch.");
-                console.log('SUCCESS!');
-            }, function(error) {
-                $('#email-confirm-msg').html("There was an error on our side, please try again later.");
-                console.log('FAILED...', error);
-            });
 
-            // Reset Form data
-            document.getElementById("contact-form").reset();
-            return false;
-    });
-}
